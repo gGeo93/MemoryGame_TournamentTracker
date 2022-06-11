e1ac0153638052bd3a67b2062ae2adb13cda9827
gGeo93 <giorgosfilippas93@gmail.com>
2022-06-11 19:08:21 +0300

    fixing pics path

--- MemoryGame/Form1.cs
+++ MemoryGame/Form1.cs
@@ -12,7 +12,7 @@ namespace MemoryGame
 {
     public partial class Form1 : Form
     {
-        readonly string partOfTheRelativePicsPath = @"..\..\..\";
+        readonly string partOfTheRelativePicsPath = @"..\..\Pics\";
         Bitmap[] nullBitmap = new Bitmap[16];
         List<Bitmap> myImages = new List<Bitmap>();
         Bitmap[] MemoryBoard = new Bitmap[16];
@@ -58,7 +58,7 @@ namespace MemoryGame
 
         private void DragTheImages()
         {
-            myImages.Add(new Bitmap(partOfTheRelativePicsPath + "pikatsu.png"));
+            myImages.Add(new Bitmap(@"..\..\Pics\pikatsu.png"));
             myImages.Add(myImages[0]);
             myImages.Add(new Bitmap(partOfTheRelativePicsPath + "bulbasar.png"));
             myImages.Add(myImages[2]);
